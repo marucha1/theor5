7144c384a943ecc6a873fb5d5d3068e3227cb57d
Mario <marucha1@etsii.upv.es>
2017-01-29 17:22:55 +0100

    theor5

---
+++ webapps/ROOT/FirefoggOriginal.js
@@ -0,0 +1,36 @@
+if(typeof(Firefogg) == 'undefined') {
+  alert('You dont have Firefogg, please go to http://firefogg.org to install it');
+  window.open('http://firefogg.org');
+}
+
+var ogg = new Firefogg();
+
+if(ogg.selectVideo()) {
+  var options = JSON.stringify({'maxSize': 320, 'videoBitrate': 500});
+  ogg.encode(options,
+             function(result, file) {
+                result = JSON.parse(result);
+                update_progress(result.progress, result.state);
+
+
+                var xhr = new XMLHttpRequest();
+                xhr.addEventListener('progress', function(e) {
+                    var progress = e.position || e.loaded, total = e.totalSize || e.total;
+                    update_progress(progress, 'uploading');
+                }, false);
+                xhr.open('post', uploadUrl, true);
+                xhr.send(file)
+             },
+             function(progress) {
+                progress = JSON.parse(progress);
+                update_progress(progress.progress, progress.state);
+             }
+  );
+}
+
+
+function update_progress(progress, text) {
+    var progressbar = document.getElementById('progressbar');
+    var relleno = parseInt(progress*10000);
+    progressbar.setAttribute("value",relleno);
+}
